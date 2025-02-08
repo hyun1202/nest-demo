0c0568022fe9b03ced73e7de70fbc6c3b2709489
hyun <apfhd0257@naver.com>
2025-02-08 18:13:04 +0900

    웹소켓 연결, 해제 로그 추가

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,28 +1,37 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer, ConnectedSocket } from '@nestjs/websockets';
+import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer, ConnectedSocket, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { Logger } from '@nestjs/common';
 
 @WebSocketGateway()
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect{
   @WebSocketServer()
   server: Server;
-
+  
   private logger = new Logger('ChatGateway');
+  
+  handleConnection(client: any, ...args: any[]) {
+    this.logger.log('client 접속');
+  }
+  
+  handleDisconnect(client: any) {
+    this.logger.log('client 접속 해제');
+  }
 
   @SubscribeMessage('join')
   handleJoinRoom(@MessageBody() room: string, @ConnectedSocket() client: Socket): void {
     this.logger.log(`Join Room: ${room}`);
     // 방 입장
     client.join(room);
+    client.broadcast.to(room).emit('chat', `join: ${room}에 입장`);
   }
-
+  
   @SubscribeMessage('message')
   handleMessage(@ConnectedSocket() client: Socket, @MessageBody() payload: {room: string, msg: string}): string {
     const {room, msg} = payload;
     this.logger.log(`Message received: room: ${room} - msg: ${msg}`);
-    // chat에 연결되어있는 모든 사람에게 전송
-    // this.server.emit('chat', `emmit: ${msg}`);
-
+    // 웹소켓에 연결되어있는 모든 사람에게 전송
+    this.server.emit('chat', `emmit: ${msg}`);
+    
     // room에 있는 모든 사람에게 전송
     this.server.to(room).emit('chat', `to: ${msg}`);
     
