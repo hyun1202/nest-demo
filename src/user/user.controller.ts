c0ac7e5efe7ef596e010d3b4328a31d29bf54eb1
hyun <apfhd0257@naver.com>
2025-02-08 16:35:47 +0900

    user 생성 api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,10 +2,16 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User as UserModel } from '@prisma/client';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  
+  @Post()
+  async signupUser(
+    @Body() userData: { name?: string; email: string },
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
 }
