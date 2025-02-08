6cf96dbfa8f059bab1609f35f3faeefbf562092b
hyun <apfhd0257@naver.com>
2025-02-08 15:27:46 +0900

    post 기본 crud api 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostController } from './post/post.controller';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, PostController],
+  imports: [PostModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
